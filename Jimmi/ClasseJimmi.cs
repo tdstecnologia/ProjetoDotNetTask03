c27479fe62620b2166ee36d201f6d6abbaaed146
Jimmi Lucas <jimmilucas@gmail.com>
2019-10-01 12:00:22 -0400

    Primeiro commit -Jimmi

---
+++ Jimmi/ClasseJimmi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Jimmi
+{
+    public class ClasseJimmi
+    {
+        public static void ExibirNome()
+        {
+            Console.WriteLine("Jimmi Lucas ...");
+        }
+    }
+}
