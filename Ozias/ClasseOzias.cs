21256adb8ccdfd0ded9e4d513cc2e9340fd024a0
ozias lemes de moraes <oziasl@gmail.com>
2019-10-02 16:58:22 -0400

    Primeira classe

---
+++ Ozias/ClasseOzias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Ozias
+{
+    public class ClasseOzias
+    {
+        public static void ExibirNome()
+        {
+            Console.WriteLine("Ozias Lemes de Moraes");
+        }
+    }
+}
