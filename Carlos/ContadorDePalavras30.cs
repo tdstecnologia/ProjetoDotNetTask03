a7107bfff333bbac120a0aa1b7b7b1ec5173c489
Carlos Butrago <carlos.butrago@gestaoprojetos.inf.br>
2019-10-02 21:20:59 -0400

    [task-30] - Contador de Palavras - 30

---
+++ Carlos/ContadorDePalavras30.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Carlos
+{
+    class ContadorDePalavras30
+    {
+
+        static string texto = "Sou o melhor no que faço, mas o que eu faço não é o melhor";
+
+        public int ContarNumeroPalavras(string frase)
+        {
+            string[] palavras = frase.Split(' ');
+            return palavras.Length;
+        }
+
+
+        public static void Main(String[] args)
+        {
+           
+            ContadorDePalavras30 classe = new ContadorDePalavras30();
+            int resultado = 0;
+
+            resultado = classe.ContarNumeroPalavras(texto);
+
+            Console.WriteLine($"Resultado: {resultado}");
+
+            Console.ReadKey();
+
+        }
+
+
+
+
+
+
+    }
+}
