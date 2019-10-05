41bb499ca98b33f01a17ecaeebfb727972266c35
João Paulo de Moraes da Costa <joao.paulo@univag.edu.br>
2019-10-04 20:49:07 -0400

    [task-44] Commit p/ Conflito - Exibir Frase - João Paulo

---
+++ JoaoPaulo/ExibirFrase44.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.JoaoPaulo
+{
+    public class ExibirFrase44
+    {
+        public static void ExibirFrase(string frase)
+        {
+            Console.WriteLine(frase);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using TDSTaskDevelopers03.Dherkyan;
+using TDSTaskDevelopers03.JoaoPaulo;
 
 namespace TDSTaskDevelopers03
 {
@@ -7,7 +7,7 @@ namespace TDSTaskDevelopers03
     {
         static void Main(string[] args)
         {
-            ExibirFrase41.ExibirFrase();
+            ExibirFrase44.ExibirFrase("Deus seja Louvado!!");
         }
     }
 }
