6ac32610114f081ef0fea024e7c9bce969890a7f
ozias50 <ozias@crea-mt.org.br>
2019-10-04 20:24:51 -0400

    Atualizacao

---
+++ Ozias/ExibirFrase47.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Ozias
+{
+    public class ExibirFrase47
+    {
+        public void ExibirFrase()
+        {
+            Console.WriteLine("Thor, sua mãe sabe que você está usando as cortinas dela ? (Homem de Ferro)");
+        }
+    }
+}
