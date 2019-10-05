f2b0942e9b13551899947eee40cfa28d283b5ccd
Carlos Butrago <carlos.butrago@gestaoprojetos.inf.br>
2019-10-04 20:49:19 -0400

    [task-43] Exibir Frase - Carlos

---
+++ Carlos/ExibirFrase43.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Carlos
+{
+    public class ExibirFrase43
+    {
+        public static void ExibirFrase()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
