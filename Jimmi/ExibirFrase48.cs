2e8cd81a12f56f544eb9932229aa0b8d68a29a80
Jimmi Lucas <56006299+jimmilucas@users.noreply.github.com>
2019-10-04 21:11:04 -0400

    Task 48 (#54)
    * Exibir Frase
    
    * excluindo pasta jimmi
    
    * inicio do projeto

---
+++ Jimmi/ExibirFrase48.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Jimmi
+{
+    class ExibirFrase48
+    {
+        public static void ExibirFrase()
+        { 
+            Console.WriteLine("Thor, sua mãe sabe que você está usando as cortinas dela?(Homem de Ferro)");
+        }
+    }
+}
