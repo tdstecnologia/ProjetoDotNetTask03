a2ecf31b5e14330a065b76e1ad0607af2bb49356
Felipe Callori <callori@gmail.com>
2019-10-04 20:20:32 -0400

    [task-45] - Exibir Frase - Felipe

---
+++ Felipe/ExibirFrase45.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Felipe
+{
+    class ExibirFrase45
+    {
+        public void ExibirFrase()
+        {
+            Console.WriteLine("Thor, sua mãe sabe que você está usando as cortinas dela?(Homem de Ferro)");
+        }
+    }
+}
