d5e54fe62e29ce53f7cbd68790641933f438a77d
João Paulo de Moraes da Costa <joao.paulo@univag.edu.br>
2019-10-07 21:54:44 -0400

    [task-75] - Conversor de Temperatura - João Paulo

---
+++ JoaoPaulo/ConversorTemperatura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTaskDevelopers03.JoaoPaulo.MinhasExceptions;
+
+namespace TDSTaskDevelopers03.JoaoPaulo
+{
+    public class ConversorTemperatura
+    {
+        public double appConversorTemperatura(double temperatura)
+        {
+            double resultado = (temperatura - 32) / 1.8;
+            return resultado;
+        }
+
+        public void ValidarTemperatura(double temperatura)
+        {
+            if (temperatura > 10)
+            {
+                throw new TemperaturaInvalidaException();
+            }
+            else
+            {
+                //Temperatura valida!!
+            }
+        }
+
+
+        public ConversorTemperatura() { }
+    }
+}
---
+++ JoaoPaulo/MinhasExceptions/TemperaturaInvalidaException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TDSTaskDevelopers03.JoaoPaulo.MinhasExceptions
+{
+    [Serializable]
+    public class TemperaturaInvalidaException : Exception
+    {
+        // Construtor padrao
+        public TemperaturaInvalidaException() : base() { }
+
+        // Construtor para mensagem
+        public TemperaturaInvalidaException(string message) : base(message) { }
+
+        // Construtor para mensagem e exception
+        public TemperaturaInvalidaException(string message, Exception exception) : base(message, exception) { }
+
+        // Construtor para serializacao
+        protected TemperaturaInvalidaException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
