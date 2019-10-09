d609844f012a2fa77534abacee889875c0e6336e
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-09 19:31:35 -0400

    Classe Calculo IMC

---
+++ Instrutor/AbstractCalculoIMC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Instrutor
+{
+    public abstract class AbstractCalculoIMC
+    {
+        public abstract void CalcularIMC();
+
+    }
+}
