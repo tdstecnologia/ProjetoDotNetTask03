d1c552d28fdf056d07080b775c9bc23d65835387
Felipe Callori <callori@gmail.com>
2019-10-09 19:51:23 -0400

    [task-88] Calculo IMC - Felipe

---
+++ Felipe/CalculoIMC88.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTaskDevelopers03.Instrutor;
+namespace TDSTaskDevelopers03.Felipe
+{
+    class CalculoIMC88 : AbstractCalculoIMC
+    {
+        public CalculoIMC88()
+        {
+        }
+
+        public override double CalcularIMC()
+        {
+            Console.WriteLine("Informe sua altura: ");
+            Double altura = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe seu peso: ");
+            Double peso = Double.Parse(Console.ReadLine());
+
+            return peso/(altura*altura);
+        }
+    }
+}
