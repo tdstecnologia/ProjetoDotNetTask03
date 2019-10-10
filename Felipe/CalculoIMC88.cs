a79bfcf51e37560df5bdcbd1f549d4b46fded347
Felipe Callori <callori@gmail.com>
2019-10-09 20:04:19 -0400

    [task-88] - Calculo IMC - Felipe

--- Felipe/CalculoIMC88.cs
+++ Felipe/CalculoIMC88.cs
@@ -4,7 +4,7 @@ using System.Text;
 using TDSTaskDevelopers03.Instrutor;
 namespace TDSTaskDevelopers03.Felipe
 {
-    class CalculoIMC88 : AbstractCalculoIMC
+    class CalculoIMC88 : AbstractCalculoIMC,IClassificacaoIMC
     {
         public CalculoIMC88()
         {
@@ -19,6 +19,36 @@ namespace TDSTaskDevelopers03.Felipe
             Double peso = Double.Parse(Console.ReadLine());
 
             return peso/(altura*altura);
+        }
+
+        public void ClassificarIMC(double imc)
+        {
+            if (imc < 18.5)
+            {
+                Console.WriteLine("MAGREZA");
+                return;
+            }
+            if(imc >=18.5 && imc <= 24.9)
+            {
+                Console.WriteLine("NORMAL");
+                return;
+            }
+            if (imc >= 25 && imc <= 29.9)
+            {
+                Console.WriteLine("SOBREPESO");
+                return;
+            }
+
+            if (imc >= 30 && imc <= 39.9)
+            {
+                Console.WriteLine("OBESIDADE");
+                return;
+            }
+            if (imc >= 40)
+            {
+                Console.WriteLine("OBESIDADE GRAVE");
+                return;
+            }
         }
     }
 }
