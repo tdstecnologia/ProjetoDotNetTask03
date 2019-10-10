73ca66ea3c97d63686f0a70b4b661a2077f390c9
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-09 20:59:18 -0400

    Classe Pessoa

---
+++ Instrutor/CalculoIMCImpl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTaskDevelopers03.Instrutor
+{
+    class CalculoIMCImpl : AbstractCalculoIMC, IClassificacaoIMC
+    {
+        public override double CalcularIMC()
+        {
+            return 0.0;
+        }
+
+        public void ClassificarIMC(double imc)
+        {
+           
+        }
+    }
+}
