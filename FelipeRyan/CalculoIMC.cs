2a7e398970f22f41e0b26b42a2ed76d97103d1b8
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-10-10 20:04:57 -0400

    [task-93] - Calculo IMC - Felipe Ryan

---
+++ FelipeRyan/CalculoIMC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTaskDevelopers03.Instrutor;
+
+namespace TDSTaskDevelopers03.FelipeRyan
+{
+    public class CalculoIMC : AbstractCalculoIMC
+    {
+        public override double CalcularIMC()
+        {
+            double peso = 80;
+            double alt = 1.70;
+     
+            double imc = peso / (alt * alt);
+            Console.WriteLine("IMC: " + imc);
+            return imc;
+
+        }
+
+    }
+}
